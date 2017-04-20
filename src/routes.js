01775d9d126ced98372cfe80671dfe496b5ce84e
jonay <casa@gmail.com>
2017-04-20 15:07:27 +0100

    Create Manage Course Page

--- src/routes.js
+++ src/routes.js
@@ -4,11 +4,14 @@ import App from './components/App';
 import HomePage from './components/home/HomePage';
 import AboutPage from './components/about/AboutPage';
 import CoursesPage from './components/courses/CoursesPage';
+import ManagerCoursePage from './components/courses/ManagerCoursePage';
 
 export default(
   <Route path="/" components={App}>
     <IndexRoute component={HomePage} />
     <Route path="courses" component={CoursesPage}/>
+    <Route path="course" component={ManagerCoursePage}/>
+    <Route path="course/:id" component={ManagerCoursePage}/>
     <Route path="about" component={AboutPage}/>
   </Route>
 );
