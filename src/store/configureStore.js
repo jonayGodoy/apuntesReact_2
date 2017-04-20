f8472ebfcab04774f58c0ada896e1f6da6535ba8
jonay <casa@gmail.com>
2017-04-20 11:52:00 +0100

    Add Thunk to Store

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,9 +1,12 @@
 import {createStore, applyMiddleware} from 'redux';
 import rootReducer from '../reducers/index';
+import reduxInmutableStateInvariant from 'redux-inmutable-state-invariant';
+import thunk from 'redux-thunk';
 
 export default function configureStore(initialState){
   return createStore(
     rootReducer,
-    initialState
+    initialState,
+    applyMiddleware(thunk, reduxInmutableStateInvariant)
   );
 }
