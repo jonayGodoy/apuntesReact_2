246b8d431744eb6292a01213c185e2a7be697bb9
jonay <casa@gmail.com>
2017-04-20 10:52:26 +0100

    Action Types Constants

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -52,7 +52,7 @@ class CoursesPage extends React.Component {
 
 CoursesPage.propTypes = {
   courses: PropTypes.array.isRequired,
-  actions: PropTypes.object.isRequired
+  actions : PropTypes.object.isRequired
 };
 
 function mapStateToProps(state, ownProps){
